e759aa9fa0509ee4dbacdb2743ffc91644535a31
pwjvergeer <49365719+pwjvergeer@users.noreply.github.com>
2019-07-08 15:54:35 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# daur_18_19
+This repo contains the materials for the "Using R for Data Analysis" @HU April - May 2019
---
+++ test1.R
@@ -0,0 +1,21 @@
+## Start demo sjizzle
+## Pim
+## 11-april
+x <- c(1:10) ##c() is a concatenate (make) function
+
+y <- c(1:10)
+z <- c(x,y) ## use ctrl+enter to run in console
+
+class(x)
+Library(tidyverse)
+Dataframe <- tibble ( x = x, y = y)
+## letters is a standard vector (alphabet)
+
+## tibble(function for dataframe) column x is x and y is y
+df <- data.frame ( x = x, y = y)
+tbl <- tibble ( x = x, y = y)
+tbl
+df
+
+##tibble vs data.frama depends on the flavour you like
+
