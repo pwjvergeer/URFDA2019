62b155a91b89b24748f52cc6d5a00a3ad078edc8
pwjvergeer <49365719+pwjvergeer@users.noreply.github.com>
2019-07-08 16:58:57 +0200

    Add files via upload

---
+++ sandbox_Pim/lesson2_bs.R
@@ -0,0 +1,33 @@
+##Always but the correct BS here
+
+#Author
+#Date
+#Purpose
+## TODO is a todo list
+test2 <- 1/200*30
+test2
+test + test2
+x <- 3*4
+
+2^10
+
+?mean
+
+##interactive session_matrix
+
+## 50 random normals mean is 2.5 stdev = 1
+x <- rnorm(50, mean = 2.5, sd  = 1)
+y <- rnorm(50, mean = 10, sd  = 2.5)
+lgl <- c(TRUE,FALSE,TRUE)
+as.integer(lgl)
+
+na<-c(1,4,NA,5)
+
+is.na(na) #check if position is "NA"
+
+as.integer(is.na(na))
+
+x <- c(x, "A")
+x
+class(x)
+mean(x)
